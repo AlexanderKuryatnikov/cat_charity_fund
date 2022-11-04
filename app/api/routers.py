eb6b0a0948679381b8bf0377ee7807880731b5a3
Alexander Kuryatnikov <asander1995@gmail.com>
2022-11-04 22:01:11 +0300

    donation router

--- app/api/routers.py
+++ app/api/routers.py
@@ -1,7 +1,14 @@
 from fastapi import APIRouter
 
-from app.api.endpoints import user_router
+from app.api.endpoints import (
+    donation_router, user_router
+)
 
 main_router = APIRouter()
 
+main_router.include_router(
+    donation_router,
+    prefix='/donation',
+    tags=['Donations'],
+)
 main_router.include_router(user_router)
