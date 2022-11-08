e0663ad6f6b9c63eafd204dcc3091844808f47d6
Alexander Kuryatnikov <asander1995@gmail.com>
2022-11-08 17:50:32 +0300

    fixed settings db_url

--- app/core/config.py
+++ app/core/config.py
@@ -4,7 +4,6 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     app_title: str = 'Благотворительный фонд поддержки котиков'
     app_description: str = 'Сбор средств на котиков'
-    database_url: str
     db_url: str = 'sqlite+aiosqlite:///./fastapi.db'
     secret: str = 'SECRET'
 
